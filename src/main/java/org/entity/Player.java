6edbc8088eb699117d384fbc7c9c162eefc67b26
ruserxd <zxcmomo0219@gmail.com>
2024-12-15 03:32:11 +0800

    feat: add Player name and showWelcomeMessage()

--- src/main/java/org/entity/Player.java
+++ src/main/java/org/entity/Player.java
@@ -3,5 +3,10 @@ package org.entity;
 /**
  * 遊戲角色*/
 public class Player {
-    
+    String name;
+
+    /*歡迎使用者的加入*/
+    public void showWelcomeMessage() {
+        System.out.println("Welcome " + this.name + " to this ARPG World");
+    }
 }
