e855ca4d5f122617341a74ea0a807af1a744120f
ruserxd <zxcmomo0219@gmail.com>
2024-12-20 03:00:15 +0800

    feat: create Player and use getName ,setName, showWelcomeMessage

--- src/main/java/org/core/Game.java
+++ src/main/java/org/core/Game.java
@@ -1,10 +1,17 @@
 package org.core;
 
+import org.entity.Player;
+
 /**
  *  遊戲主程式
  * */
 public class Game {
     public static void main(String[] args) {
         System.out.println("Let the games begin!!");
+
+        Player player = new Player();
+        player.setName("ruserxd");
+        System.out.printf("Auto creating a %s user", player.getName());
+        player.showWelcomeMessage();
     }
 }
