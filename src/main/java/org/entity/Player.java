ee0a17b53faf23d7289722b492bb6cb3bf4bdb0a
ruserxd <zxcmomo0219@gmail.com>
2024-12-20 02:59:23 +0800

    feat: add Player getName and setName

--- src/main/java/org/entity/Player.java
+++ src/main/java/org/entity/Player.java
@@ -5,6 +5,14 @@ package org.entity;
 public class Player {
     String name;
 
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     /*歡迎使用者的加入*/
     public void showWelcomeMessage() {
         System.out.println("Welcome " + this.name + " to this ARPG World");
